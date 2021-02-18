5cf858ab9d0804836cedc00ef4507f345b251862
Lars Steen <lats@aarhus.dk>
2021-02-18 13:31:47 +0100

    NSEK-102: Removing stimulus

--- assets/app.js
+++ assets/app.js
@@ -8,9 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss'
 
-// start the Stimulus application
-// import './bootstrap'
-
 const $ = require('jquery');
 require('bootstrap');
 
