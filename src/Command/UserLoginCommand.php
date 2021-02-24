ef20ec1d9accc49d2361d771515ea741fa4f72a0
Jeppe Kuhlmann Andersen <jeppeka@Jeppes-MacBook-Pro.local>
2021-02-24 15:13:38 +0100

    NSEK/105: Added app:user-login command, does not give proper url yet

---
+++ src/Command/UserLoginCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+//use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class UserLoginCommand extends Command
+{
+    protected static $defaultName = 'app:user-login';
+    protected static $defaultDescription = 'Add a short description for your command';
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription(self::$defaultDescription)
+            ->addArgument('arg1', InputArgument::REQUIRED, 'Argument description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+        // Call some method or service to create the absolute url to be output via success
+        $loginPage = $this->urlGenerator->generate('default', [
+            'token' => $arg1,
+        ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $io->success('URL:   '.$loginPage);
+
+        return Command::SUCCESS;
+    }
+}
