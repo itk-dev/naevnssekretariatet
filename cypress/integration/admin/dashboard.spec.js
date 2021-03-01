b69887ab1c8bde084c88f603c1dfa05bcd15ca88
Lars Steen <lats@aarhus.dk>
2021-03-01 14:43:58 +0100

    NSEK-112: Applying coding standards

--- cypress/integration/admin/dashboard.spec.js
+++ cypress/integration/admin/dashboard.spec.js
@@ -1,6 +1,6 @@
-/* global describe it expect cy */
+/* global describe it cy */
 describe('End-to-end tests for the admin dashboard', () => {
-    it('Shows the admin dashboard', () => {
-        cy.visit('/admin')
-    })
+  it('Shows the admin dashboard', () => {
+    cy.visit('/admin')
+  })
 })
