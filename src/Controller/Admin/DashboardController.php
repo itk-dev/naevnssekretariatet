8300e688c7c688880aac39af6caed6956884ef92
Jeppe Kuhlmann Andersen <jeppeka@Jeppes-MacBook-Pro.local>
2021-04-13 13:54:36 +0200

    NSEK118: Changes to layout and crud configuration

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Board;
 use App\Entity\BoardMember;
 use App\Entity\Municipality;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -31,8 +33,19 @@ class DashboardController extends AbstractDashboardController
     public function configureCrud(): Crud
     {
         return Crud::new()
-        ->setPaginatorPageSize(10)
-        ->setPaginatorRangeSize(2);
+            ->setPaginatorPageSize(10)
+            ->setPaginatorRangeSize(2)
+            ;
+    }
+
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::DELETE)
+            ->add(Crud::PAGE_EDIT, Action::INDEX)
+            ->add(Crud::PAGE_NEW, Action::INDEX)
+            ;
     }
 
     public function configureMenuItems(): iterable
