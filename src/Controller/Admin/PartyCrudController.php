ae9bf96bf0730a8ec1c6ad4b077e6fd3f427e878
Jeppe Kuhlmann Andersen <jeppeka@Jeppes-MacBook-Pro.local>
2021-04-14 14:58:56 +0200

    NSEK-119: Party fields and labels added

--- src/Controller/Admin/PartyCrudController.php
+++ src/Controller/Admin/PartyCrudController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Party;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PartyCrudController extends AbstractCrudController
 {
@@ -12,14 +15,24 @@ class PartyCrudController extends AbstractCrudController
         return Party::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('new', 'Add party')
+            ->setEntityLabelInSingular('Party')
+            ->setEntityLabelInPlural('Parties')
+            ->setSearchFields(['firstName', 'lastName'])
+            ->setDefaultSort(['firstName' => 'ASC'])
+            ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('firstName', 'First name');
+        yield TextField::new('lastName', 'Last name');
+        yield TextField::new('address', 'Address');
+        yield TextField::new('phoneNumber', 'Phone Number');
+        yield BooleanField::new('isNameAndAddressProtected', 'Name and address protected');
+        yield TextField::new('journalNumber', 'Journal number');
     }
-    */
 }
