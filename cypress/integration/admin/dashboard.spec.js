e13bf054a4e15ef3c9f7c127d32f7322e4c6dd53
Lars Steen <lats@aarhus.dk>
2021-06-02 08:42:13 +0200

    Dashboard test now using the e2e authenticator

--- cypress/integration/admin/dashboard.spec.js
+++ cypress/integration/admin/dashboard.spec.js
@@ -1,5 +1,11 @@
+/* @todo There should be a better way for authenticating users */
+
 /* global describe it cy */
 describe('End-to-end tests for the admin dashboard', () => {
+  before(() => {
+    cy.visit('/?masquerade=test@example.com')
+  })
+
   it('Shows the admin dashboard', () => {
     cy.visit('/admin')
   })
