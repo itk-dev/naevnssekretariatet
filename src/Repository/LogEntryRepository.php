55056df2ecf1be30a18336ad210ad3062b1b0a28
jekuaitk <jekua@aarhus.dk>
2021-06-24 15:19:46 +0200

    TVIST1-253: Removed unnecessary auto generated code from LogEntryRepository

--- src/Repository/LogEntryRepository.php
+++ src/Repository/LogEntryRepository.php
@@ -18,33 +18,4 @@ class LogEntryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, LogEntry::class);
     }
-
-    // /**
-    //  * @return LogEntry[] Returns an array of LogEntry objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('l')
-            ->andWhere('l.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('l.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?LogEntry
-    {
-        return $this->createQueryBuilder('l')
-            ->andWhere('l.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
