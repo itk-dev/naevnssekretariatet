1a7817a40f38ed6f73ec7bbdf8ffe21e11824a3e
jekuaitk <jekua@aarhus.dk>
2021-06-28 09:59:57 +0200

    TVIST1-256: Added tests for CaseManager service

---
+++ tests/Service/CaseManagerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\CaseEntity;
+use App\Repository\CaseEntityRepository;
+use App\Service\CaseManager;
+use PHPUnit\Framework\TestCase;
+
+class CaseManagerTest extends TestCase
+{
+    private $mockCaseRepository;
+    private $caseManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockCaseRepository = $this->createMock(CaseEntityRepository::class);
+
+        $this->caseManager = new CaseManager($this->mockCaseRepository);
+    }
+
+    public function testGenerateCaseNumberFirstCase()
+    {
+        $this->mockCaseRepository
+            ->expects($this->once())
+            ->method('findLatestCase')
+            ->willReturn(null);
+
+        $date = new \DateTime();
+        $year = $date->format('Y');
+
+        $result = $this->caseManager->generateCaseNumber();
+
+        $this->assertSame($year.'-0001', $result);
+    }
+
+    public function testGenerateCaseNumberpreviousYear()
+    {
+        $mockCase = $this->createMock(CaseEntity::class);
+
+        $this->mockCaseRepository
+            ->expects($this->once())
+            ->method('findLatestCase')
+            ->willReturn($mockCase);
+
+        $mockDate = $this->createMock(\DateTime::class);
+
+        $mockCase
+            ->expects($this->once())
+            ->method('getCreatedAt')
+            ->willReturn($mockDate);
+
+        $date = new \DateTime();
+        $currentYear = $date->format('Y');
+
+        $previousYear = (int) $currentYear - 1;
+        $previousYearString = (string) $previousYear;
+
+        $mockDate
+            ->expects($this->once())
+            ->method('format')
+            ->with('Y')
+            ->willReturn($previousYearString);
+
+        $result = $this->caseManager->generateCaseNumber();
+
+        $this->assertSame($currentYear.'-0001', $result);
+    }
+
+    public function testGetIncrementedCaseCounter()
+    {
+        $testCaseNumber = '2021-0001';
+
+        $result = $this->caseManager->getIncrementedCaseCounter($testCaseNumber);
+
+        $this->assertSame('0002', $result);
+    }
+}
