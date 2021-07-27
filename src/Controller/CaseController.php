9a574903dade2c805e136408f41e8dee233ea5c1
jekuaitk <jekua@aarhus.dk>
2021-07-27 10:32:08 +0200

    TVIST1-233: Added case summary route and added templates for case summary

--- src/Controller/CaseController.php
+++ src/Controller/CaseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CaseEntityRepository;
+use DeepCopy\Filter\ReplaceFilter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +19,17 @@ class CaseController extends AbstractController
     public function index(): Response
     {
         return $this->render('case/index.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/summary", name="case_summary", methods={"GET"})
+     */
+    public function summary(): Response
+    {
+        return $this->render('case/summary.html.twig', [
+
         ]);
     }
 }
