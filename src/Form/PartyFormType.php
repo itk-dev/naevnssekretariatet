514cf1702e126939f04b0ac25d14d63b69e33554
jekuaitk <jekua@aarhus.dk>
2021-09-07 13:03:05 +0200

    TVIST1-257: Applied coding standards

--- src/Form/PartyFormType.php
+++ src/Form/PartyFormType.php
@@ -5,9 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PartyFormType extends AbstractType
 {
