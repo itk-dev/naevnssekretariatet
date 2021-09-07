30ba7050e0aff5606cc6d8678190e574e8a26ac1
jekuaitk <jekua@aarhus.dk>
2021-09-07 12:39:21 +0200

    TVIST1-251: Applied coding standards

--- src/Form/DocumentForm.php
+++ src/Form/DocumentForm.php
@@ -7,7 +7,6 @@ use App\Entity\DocumentType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
