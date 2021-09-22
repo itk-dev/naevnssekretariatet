6cfd8288f99a3a06e34f75a5e3f3ac2fa2e51971
Lars Steen <lats@aarhus.dk>
2021-09-22 15:25:58 +0200

    TVIST1-306: Applying coding standards

--- assets/case/new.js
+++ assets/case/new.js
@@ -1,17 +1,19 @@
-var $board = $('#case_entity_board');
+/* global $ */
+
+const $board = $('#case_entity_board')
 $board.change(function () {
-    var $form = $(this).closest('form');
-    var data = {};
-    data[$board.attr('name')] = $board.val();
-    // Submit data via AJAX to the form's action path.
-    $.ajax({
-        url: $form.attr('action'),
-        type: $form.attr('method'),
-        data: data,
-        success: function (html) {
-            $('#case_entity_caseEntity').replaceWith(
-                $(html).find('#case_entity_caseEntity')
-            );
-        }
-    });
-});
+  const $form = $(this).closest('form')
+  const data = {}
+  data[$board.attr('name')] = $board.val()
+  // Submit data via AJAX to the form's action path.
+  $.ajax({
+    url: $form.attr('action'),
+    type: $form.attr('method'),
+    data: data,
+    success: function (html) {
+      $('#case_entity_caseEntity').replaceWith(
+        $(html).find('#case_entity_caseEntity')
+      )
+    }
+  })
+})
