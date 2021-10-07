692bd69c8bd7ca646e9d0140e246d869d83d6e9e
jekuaitk <jekua@aarhus.dk>
2021-10-07 11:27:05 +0200

    TVIST1-297: Appled js coding standards

--- assets/agenda_item/new.js
+++ assets/agenda_item/new.js
@@ -2,18 +2,18 @@
 
 const $type = $('#agenda_item_type')
 $type.change(function () {
-    const $form = $(this).closest('form')
-    const data = {}
-    data[$type.attr('name')] = $type.val()
-    // Submit data via AJAX to the form's action path.
-    $.ajax({
-        url: $form.attr('action'),
-        type: $form.attr('method'),
-        data: data,
-        success: function (html) {
-            $('#agenda_item_agendaItem').replaceWith(
-                $(html).find('#agenda_item_agendaItem')
-            )
-        }
-    })
+  const $form = $(this).closest('form')
+  const data = {}
+  data[$type.attr('name')] = $type.val()
+  // Submit data via AJAX to the form's action path.
+  $.ajax({
+    url: $form.attr('action'),
+    type: $form.attr('method'),
+    data: data,
+    success: function (html) {
+      $('#agenda_item_agendaItem').replaceWith(
+        $(html).find('#agenda_item_agendaItem')
+      )
+    }
+  })
 })
