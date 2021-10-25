efd761e992271aef4ff8095bf4db84fb2bf302cd
jekuaitk <jekua@aarhus.dk>
2021-10-25 12:01:52 +0200

    TVIST1-144: Switched to OpenId Connect Bundle for AD login

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,13 +5,9 @@ namespace App\Controller;
 use App\Entity\Municipality;
 use App\Entity\User;
 use App\Repository\MunicipalityRepository;
-use App\Security\OpenIdConfigurationProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Security;
 
 class DefaultController extends AbstractController
@@ -36,29 +32,6 @@ class DefaultController extends AbstractController
             'municipalities' => $municipalities,
             'favorite_municipality' => $favoriteMunicipality,
         ]);
-    }
-
-    /**
-     * @Route("/login", name="login")
-     */
-    public function login(SessionInterface $session, array $openIdProviderOptions = []): Response
-    {
-        // Hackish until new bundle fixes this
-        $redirectUrl = 'https:'.$this->generateUrl('default', [], UrlGeneratorInterface::NETWORK_PATH);
-
-        if ('/' === substr($redirectUrl, -1)) {
-            $redirectUrl = substr($redirectUrl, 0, -1);
-        }
-
-        $provider = new OpenIdConfigurationProvider([
-                'redirectUri' => $redirectUrl,
-            ] + $openIdProviderOptions);
-
-        $authUrl = $provider->getAuthorizationUrl();
-
-        $session->set('oauth2state', $provider->getState());
-
-        return new RedirectResponse($authUrl);
     }
 
     /**
