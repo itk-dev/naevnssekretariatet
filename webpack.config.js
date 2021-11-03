b5b4a776c723c21a8b882e97dc93c7281485edd2
jekuaitk <jekua@aarhus.dk>
2021-11-03 15:19:21 +0100

    TVIST1-297: Use js to update filter and add basic reset filter button

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
   .addEntry('app', './assets/app.js')
   .addEntry('agenda_item_new', './assets/agenda_item/new.js')
+  .addEntry('agenda_filter', './assets/agenda/processFilter.js')
 
 // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
 //  .enableStimulusBridge('./assets/controllers.json')
