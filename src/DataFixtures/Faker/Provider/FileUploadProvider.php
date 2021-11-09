c6d8a6fc64629b8a05cf4dd3c5bb309608a25c7a
Mikkel Ricky <rimi@aarhus.dk>
2021-11-09 14:31:05 +0100

    TVIST1-287: Added file upload to fixtures

---
+++ src/DataFixtures/Faker/Provider/FileUploadProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DataFixtures\Faker\Provider;
+
+use Faker\Generator;
+use Faker\Provider\Base;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Vich\UploaderBundle\Handler\UploadHandler;
+use Vich\UploaderBundle\Mapping\PropertyMapping;
+use Vich\UploaderBundle\Mapping\PropertyMappingFactory;
+
+class FileUploadProvider extends Base
+{
+    /**
+     * @var PropertyMappingFactory
+     */
+    private $propertyMappingFactory;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    public function __construct(Generator $generator, ContainerInterface $container, PropertyMappingFactory $propertyMappingFactory, Filesystem $filesystem)
+    {
+        parent::__construct($generator);
+        $this->container = $container;
+        $this->propertyMappingFactory = $propertyMappingFactory;
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * Upload a file.
+     *
+     * @param string $path The file path relative to the fixtures directory.
+     * @param string $property The object property.
+     * @param string $className The object class name.
+     * @return string The file path relative to the configured upload destination.
+     */
+    public function uploadFile(string $path, string $property, string $className)
+    {
+        $mapping = $this->propertyMappingFactory->fromField([], $property, $className);
+        $mapping->getUploadDestination();
+
+        $path = ltrim($path, '/');
+        $sourcePath = $this->container->getParameter('kernel.project_dir').'/fixtures/'.$path;
+        $targetPath = $mapping->getUploadDestination().'/'.$path;
+
+        if (!file_exists($sourcePath)) {
+            throw new \InvalidArgumentException(sprintf('File source path %s does not exist', $sourcePath));
+        }
+        $this->filesystem->copy($sourcePath, $targetPath);
+
+        return $path;
+    }
+}
