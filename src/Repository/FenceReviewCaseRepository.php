d1286af4929f1fcf99113e178a33a22812e8c200
jekuaitk <jekua@aarhus.dk>
2021-11-10 14:36:55 +0100

    TVIST1-238: Removed unnecessary comments and commented code

--- src/Repository/FenceReviewCaseRepository.php
+++ src/Repository/FenceReviewCaseRepository.php
@@ -18,33 +18,4 @@ class FenceReviewCaseRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, FenceReviewCase::class);
     }
-
-    // /**
-    //  * @return FenceReviewCase[] Returns an array of FenceReviewCase objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('f.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?FenceReviewCase
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
--- src/Repository/RentBoardCaseRepository.php
+++ src/Repository/RentBoardCaseRepository.php
@@ -18,33 +18,4 @@ class RentBoardCaseRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, RentBoardCase::class);
     }
-
-    // /**
-    //  * @return RentBoardCase[] Returns an array of RentBoardCase objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('r.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?RentBoardCase
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
