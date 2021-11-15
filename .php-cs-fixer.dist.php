f9adf2f27891ca0660e6a136f01729b081cd966b
jekuaitk <jekua@aarhus.dk>
2021-11-15 10:02:28 +0100

    TVIST1-407: Added multiline semicolon php-cs-fixer rule

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,6 +8,7 @@ $config = new PhpCsFixer\Config();
 
 return $config->setRules([
         '@Symfony' => true,
+        'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
     ])
     ->setFinder($finder)
 ;
