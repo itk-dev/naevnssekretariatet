0e0393d771a6f4d0a90bd6033b53b219c31b4a9e
jekuaitk <jekua@aarhus.dk>
2021-11-19 15:09:03 +0100

    TVIST1-297: Code cleanup

--- src/Form/AgendaEditType.php
+++ src/Form/AgendaEditType.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
