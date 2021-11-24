8af4ec28b3a63c39c3edb20b36c67965258b1790
jekuaitk <jekua@aarhus.dk>
2021-11-24 08:39:35 +0100

    TVIST1-297: Attempt at fixing issue

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
-module.exports = Encore.getWebpackConfig()
-
 Encore
 // directory where compiled assets will be stored
   .setOutputPath('public/build/')
@@ -80,3 +78,6 @@ Encore
   .copyFiles({
     from: './assets/images'
   })
+
+
+module.exports = Encore.getWebpackConfig()
