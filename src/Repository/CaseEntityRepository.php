67a118f7e1ca8777ed08aadd4796002834ce130f
jekuaitk <jekua@aarhus.dk>
2021-11-26 13:40:29 +0100

    TVIST1-424: Update case number generation and fixtures

--- src/Repository/CaseEntityRepository.php
+++ src/Repository/CaseEntityRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Entity\CaseEntity;
-use App\Entity\Municipality;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -20,11 +19,9 @@ class CaseEntityRepository extends ServiceEntityRepository
         parent::__construct($registry, CaseEntity::class);
     }
 
-    public function findLatestCaseByMunicipality(Municipality $municipality): ?CaseEntity
+    public function findLatestCase(): ?CaseEntity
     {
         return $this->createQueryBuilder('c')
-            ->where('c.municipality = :municipality')
-            ->setParameter('municipality', $municipality->getId()->toBinary())
             ->orderBy('c.createdAt', 'DESC')
             ->setMaxResults(1)
             ->getQuery()
--- src/Service/CaseManager.php
+++ src/Service/CaseManager.php
@@ -27,24 +27,23 @@ class CaseManager
 
     public function generateCaseNumber(Municipality $municipality): string
     {
-        // Find latest case in respective municipality
-        $case = $this->caseRepository->findLatestCaseByMunicipality($municipality);
+        $case = $this->caseRepository->findLatestCase();
 
         // Get current year
         $date = new \DateTime();
-        $currentYear = $date->format('Y');
+        $currentYear = $date->format('y');
 
         // Check if first case
         if (null === $case) {
-            return $currentYear.'-0001';
+            return $currentYear.'-000001';
         }
 
         // Get the year latest case is from
-        $latestCaseYear = $case->getCreatedAt()->format('Y');
+        $latestCaseYear = $case->getCreatedAt()->format('y');
 
         // Check if we have entered a new year
         if ($currentYear !== $latestCaseYear) {
-            return $currentYear.'-0001';
+            return $currentYear.'-000001';
         }
 
         return $latestCaseYear.'-'.$this->getIncrementedCaseCounter($case->getCaseNumber());
@@ -57,7 +56,7 @@ class CaseManager
         $counter = (int) substr($caseNumber, $positionOfDash + 1);
         $incrementedCounter = $counter + 1;
 
-        return str_pad(strval($incrementedCounter), 4, '0', STR_PAD_LEFT);
+        return str_pad(strval($incrementedCounter), 6, '0', STR_PAD_LEFT);
     }
 
     public function newCase(CaseEntity $caseEntity, Board $board): CaseEntity
