721bd62c4165057daafb0e355fe895e55c5dc21c
jekuaitk <jekua@aarhus.dk>
2021-11-30 11:21:04 +0100

    TVIST1-297: Removed unused AgendaHelper method

--- src/Service/AgendaHelper.php
+++ src/Service/AgendaHelper.php
@@ -6,15 +6,6 @@ use App\Entity\Agenda;
 
 class AgendaHelper
 {
-    public function findAvailableBoardMembers(Agenda $agenda): array
-    {
-        $allBoardMembers = $agenda->getBoard()->getBoardMembers()->toArray();
-
-        $currentBoardMembersOnAgenda = $agenda->getBoardmembers()->toArray();
-
-        return array_diff($allBoardMembers, $currentBoardMembersOnAgenda);
-    }
-
     public function getFormOptionsForAgenda(Agenda $agenda): array
     {
         $options = [];
