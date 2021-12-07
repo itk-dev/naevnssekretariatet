72fa77127eaa30220e53c6d3805a9f322f636f08
jekuaitk <jekua@aarhus.dk>
2021-12-07 13:27:08 +0100

    TVIST1-17: Added command for updating deadline booleans on CaseEntity

--- src/Service/CaseManager.php
+++ src/Service/CaseManager.php
@@ -6,11 +6,15 @@ use App\Entity\Board;
 use App\Entity\CaseEntity;
 use App\Entity\Municipality;
 use App\Repository\CaseEntityRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\Lock\LockFactory;
 
-class CaseManager
+class CaseManager implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
     /**
      * @var CaseEntityRepository
      */
@@ -98,5 +102,48 @@ class CaseManager
         $lock->release();
 
         return $caseEntity;
+    }
+
+    public function updateDeadlineBooleans($isDryRun)
+    {
+        // We do this in batches as there might be a large number of cases
+        // @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.9/reference/batch-processing.html#iterating-results
+        $batchSize = 20;
+        $i = 1;
+        $query = $this->caseRepository->createQueryBuilder('c')->getQuery();
+
+        $today = new DateTime('today');
+
+        $this->logger->info('Today: '.$today->format('d/m/Y'));
+
+        foreach ($query->toIterable() as $case) {
+            assert($case instanceof CaseEntity);
+            $hearingDeadline = $case->getFinishHearingDeadline();
+            $processingDeadline = $case->getFinishProcessingDeadline();
+
+            if ($hearingDeadline < $today) {
+                $this->logger->info('Changing case '.$case->getCaseNumber().' with hearing deadline: '.$hearingDeadline->format('d/m/Y'));
+
+                if (!$isDryRun) {
+                    $case->setHasReachedHearingDeadline(true);
+                }
+
+                if ($processingDeadline < $today) {
+                    $this->logger->info('Changing case '.$case->getCaseNumber().' with processing deadline: '.$hearingDeadline->format('d/m/Y'));
+
+                    if (!$isDryRun) {
+                        $case->setHasReachedProcessingDeadline(true);
+                    }
+                }
+            }
+            ++$i;
+
+            if (($i % $batchSize) === 0 && !$isDryRun) {
+                $this->entityManager->flush(); // Executes all updates.
+                $this->entityManager->clear(); // Detaches all objects from Doctrine!
+            }
+        }
+
+        $this->entityManager->flush();
     }
 }
