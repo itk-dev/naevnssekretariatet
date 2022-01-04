2233ef0f890745be5a52bbe1219973ca29048026
Mikkel Ricky <rimi@aarhus.dk>
2022-01-04 11:07:55 +0100

    Handled uuids

--- src/Entity/CasePresentation.php
+++ src/Entity/CasePresentation.php
@@ -5,8 +5,7 @@ namespace App\Entity;
 use App\Logging\LoggableEntityInterface;
 use App\Repository\CasePresentationRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\IdGenerator\UuidV4Generator;
-use Symfony\Component\Uid\UuidV4;
+use Symfony\Component\Uid\Uuid;
 
 /**
  * @ORM\Entity(repositoryClass=CasePresentationRepository::class)
@@ -17,8 +16,6 @@ class CasePresentation implements LoggableEntityInterface
     /**
      * @ORM\Id
      * @ORM\Column(type="uuid", unique=true)
-     * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class=UuidV4Generator::class)
      */
     private $id;
 
@@ -32,7 +29,12 @@ class CasePresentation implements LoggableEntityInterface
      */
     private $caseEntity;
 
-    public function getId(): ?UuidV4
+    public function __construct()
+    {
+        $this->id = Uuid::v4();
+    }
+
+    public function getId(): ?Uuid
     {
         return $this->id;
     }
--- src/Entity/Reminder.php
+++ src/Entity/Reminder.php
@@ -4,8 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ReminderRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\IdGenerator\UuidV4Generator;
-use Symfony\Component\Uid\UuidV4;
+use Symfony\Component\Uid\Uuid;
 
 /**
  * @ORM\Entity(repositoryClass=ReminderRepository::class)
@@ -15,8 +14,6 @@ class Reminder
     /**
      * @ORM\Id
      * @ORM\Column(type="uuid", unique=true)
-     * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class=UuidV4Generator::class)
      */
     private $id;
 
@@ -46,7 +43,12 @@ class Reminder
      */
     private $createdBy;
 
-    public function getId(): ?UuidV4
+    public function __construct()
+    {
+        $this->id = Uuid::v4();
+    }
+
+    public function getId(): ?Uuid
     {
         return $this->id;
     }
