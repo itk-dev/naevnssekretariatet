6f41214283be560e796f0997f11615aa8f31a1d3
Lars Steen Risom <lats@aarhus.dk>
2022-01-07 13:50:31 +0100

    Adding doctrine handler for monolog

---
+++ src/Monolog/LogEntry.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Monolog;
+
+use App\Entity\User;
+use DateTime;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LogEntryRepository::class)
+ * @ORM\Table(name="log")
+ */
+class LogEntry
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $message;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private array $context = [];
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private int $level;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private string $levelName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $channel;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private array $extra = [];
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTimeInterface $createdAt;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $formatted;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     */
+    private User $user;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getContext(): ?array
+    {
+        return $this->context;
+    }
+
+    public function setContext(array $context): self
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getLevelName(): ?string
+    {
+        return $this->levelName;
+    }
+
+    public function setLevelName(string $levelName): self
+    {
+        $this->levelName = $levelName;
+
+        return $this;
+    }
+
+    public function getChannel(): ?string
+    {
+        return $this->channel;
+    }
+
+    public function setChannel(string $channel): self
+    {
+        $this->channel = $channel;
+
+        return $this;
+    }
+
+    public function getExtra(): ?array
+    {
+        return $this->extra;
+    }
+
+    public function setExtra(array $extra): self
+    {
+        $this->extra = $extra;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getFormatted(): ?string
+    {
+        return $this->formatted;
+    }
+
+    public function setFormatted(string $formatted): self
+    {
+        $this->formatted = $formatted;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
