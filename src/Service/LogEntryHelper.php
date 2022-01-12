7ceb9705cba90596f73687cfbac9bc5a1460e60d
Mikkel Ricky <rimi@aarhus.dk>
2022-01-12 21:26:02 +0100

    TVIST1-252: Added log entry helper options

--- src/Service/LogEntryHelper.php
+++ src/Service/LogEntryHelper.php
@@ -3,24 +3,58 @@
 namespace App\Service;
 
 use App\Entity\LogEntry;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Symfony\Component\Translation\DataCollectorTranslator;
+use Symfony\Component\Translation\MessageCatalogueInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LogEntryHelper
 {
-    public function __construct(private PropertyAccessorInterface $propertyAccessor, private TranslatorInterface $translator)
+    private array $options;
+
+    public function __construct(private PropertyAccessorInterface $propertyAccessor, private TranslatorInterface $translator, array $logEntryHelperOptions)
     {
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+        $this->options = $resolver->resolve($logEntryHelperOptions);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setRequired('log_entry_display')
+            ->setDefault('log_entry_display', function (OptionsResolver $logEntryResolver) {
+                $logEntryResolver
+                    ->setRequired('exclude_keys')
+                    ->setAllowedValues('exclude_keys', fn ($value) => is_array($value) && array_is_list($value))
+                    ->setInfo('exclude_keys', 'log_entry_display.exclude_keys must a list of values')
+
+                    ->setRequired('translation_domains')
+                    ->setAllowedTypes('translation_domains', 'array')
+                ;
+            })
+        ;
     }
 
     public function getDisplayData(LogEntry $logEntry): array
     {
         $data = $logEntry->getData();
-        unset($data['updatedAt']);
+        foreach ($this->options['log_entry_display']['exclude_keys'] as $key) {
+            if (preg_match('/([\/#~]).+\1$/', $key)) {
+                // It looks like a regex.
+                $data = array_filter($data, fn ($name) => 0 === preg_match($key, $name), ARRAY_FILTER_USE_KEY);
+            } else {
+                unset($data[$key]);
+            }
+        }
 
         return $this->getTranslatedData($data);
     }
 
+    /**
+     * Translate keys in data.
+     */
     private function getTranslatedData(array $data): array
     {
         $translatedData = [];
@@ -32,13 +66,20 @@ class LogEntryHelper
         return $translatedData;
     }
 
+    /**
+     * Translate a property.
+     */
     private function translatePropertyPath(string $propertyPath): string
     {
         $messages = $this->getMessages();
-        // Keep only letters, digits and dots and make lowercase.
-        $messageKey = strtolower(preg_replace('/[^[:alnum:].]+/', '', $propertyPath));
 
-        return $messages[$messageKey] ?? $propertyPath;
+        // Check for message matching the lowercase property path.
+        //
+        // This assumes that the translation of a property path matches the path
+        // when all non-numeric characters are removed from the translation,
+        // i.e. we assume that the property "leaseSize", say, has a translation
+        // like "Lease size".
+        return $messages[strtolower($propertyPath)] ?? $propertyPath;
     }
 
     private ?array $messages = null;
@@ -46,11 +87,15 @@ class LogEntryHelper
     /**
      * Get list of lowercase messages.
      *
-     * The list contains all messages from the "case" and "address" domains.
+     * The list contains all messages from the "case" and "address" domains with
+     * non-alpha-numeric characters removed and converted to lowercase, i.e.
+     *
+     *   "Complaint category" is converted to "complaintcategory"
+     *   "Lease size" is converted to "leasesize"
      *
      * Furthermore combinations from "case" and "address", e.g.
-     * "leaseaddress.street", are included to easily translate embedded
-     * properties.
+     * "leaseaddress.street", are combined and included to easily translate
+     * embedded properties.
      */
     private function getMessages(): array
     {
@@ -60,19 +105,26 @@ class LogEntryHelper
                 $catalogue = $this->translator->getCatalogue();
                 // @todo Use class metadata to improve this.
                 // @todo Cache the built translations for speedup.
-                foreach ($catalogue->all('case') as $key => $value) {
-                    // Keep only letters and digits and make lowercase.
-                    $caseKey = strtolower(preg_replace('/[^[:alnum:]]+/', '', $key));
-                    $this->messages[$caseKey] = $value;
-                    foreach ($catalogue->all('address') as $addressKey => $addressValue) {
-                        $addressKey = strtolower(preg_replace('/[^[:alnum:]]+/', '', $addressKey));
-                        $this->messages[$addressKey] = $addressValue;
-                        $this->messages[$caseKey.'.'.$addressKey] = sprintf('%s (%s)', $addressValue, $value);
-                    }
+                foreach ($this->options['log_entry_display']['translation_domains'] as $domain => $subDomains) {
+                    $this->collectMessages($domain, $subDomains, $catalogue);
                 }
             }
         }
 
         return $this->messages;
+    }
+
+    private function collectMessages(string $domain, ?array $subDomains, MessageCatalogueInterface $catalogue, string $messagePrefix = '')
+    {
+        foreach ($catalogue->all($domain) as $key => $message) {
+            // Keep only letters and digits and make lowercase.
+            $messageKey = strtolower(preg_replace('/[^[:alnum:]]+/', '', $key));
+            $this->messages[$messagePrefix.$messageKey] = $message;
+            if (is_array($subDomains)) {
+                foreach ($subDomains as $subDomain => $subSubDomains) {
+                    $this->collectMessages($subDomain, $subSubDomains, $catalogue, $messageKey.'.');
+                }
+            }
+        }
     }
 }
