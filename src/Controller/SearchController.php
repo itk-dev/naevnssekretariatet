61c09da5077d9e9525e41f77d6b814e36062da3e
jekuaitk <jekua@aarhus.dk>
2022-01-17 14:52:07 +0100

    TVIST1-14: Added SearchController and template

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search")
+     */
+    public function index(Request $request)
+    {
+        $search = $request->query->get('search');
+
+        return $this->render('search/index.html.twig');
+    }
+}
