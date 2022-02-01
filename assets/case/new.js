e0e5fbef572ec17563dd80eca34ab8b03db50f7c
jekuaitk <jekua@aarhus.dk>
2022-02-01 14:10:32 +0100

    TVIST1-502: Double dynamic case creation

--- assets/case/new.js
+++ assets/case/new.js
@@ -1,20 +1,39 @@
 /* global $, Event */
 
-const $board = $('#case_entity_board')
-$board.change(function () {
+const $municipality = $('#case_entity_municipality')
+$municipality.change(function () {
   const $form = $(this).closest('form')
   const data = {}
-  data[$board.attr('name')] = $board.val()
+  data[$municipality.attr('name')] = $municipality.val()
   // Submit data via AJAX to the form's action path.
   $.ajax({
     url: $form.attr('action'),
     type: $form.attr('method'),
     data: data,
     success: function (html) {
-      $('#case_entity_caseEntity').replaceWith(
-        $(html).find('#case_entity_caseEntity')
+      $('#case_entity_board').replaceWith(
+        $(html).find('#case_entity_board')
       )
       window.dispatchEvent(new Event('ajaxload'))
     }
   })
 })
+//
+// const $board = $('#case_entity_board')
+// $board.change(function () {
+//   const $form = $(this).closest('form')
+//   const data = {}
+//   data[$board.attr('name')] = $board.val()
+//   // Submit data via AJAX to the form's action path.
+//   $.ajax({
+//     url: $form.attr('action'),
+//     type: $form.attr('method'),
+//     data: data,
+//     success: function (html) {
+//       $('#case_entity_caseEntity').replaceWith(
+//         $(html).find('#case_entity_caseEntity')
+//       )
+//       window.dispatchEvent(new Event('ajaxload'))
+//     }
+//   })
+// })
--- src/Form/CaseEntityType.php
+++ src/Form/CaseEntityType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Board;
+use App\Entity\Municipality;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
@@ -26,36 +28,46 @@ class CaseEntityType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('board', EntityType::class, [
-            'class' => Board::class,
-            'label' => $this->translator->trans('Board', [], 'case'),
-            'placeholder' => $this->translator->trans('Choose a board', [], 'case'),
+        $builder->add('municipality', EntityType::class, [
+            'class' => Municipality::class,
+            'label' => $this->translator->trans('Municipality', [], 'case'),
+            'placeholder' => $this->translator->trans('Choose a municipality', [], 'case'),
         ]);
 
-        $formModifier = function (FormInterface $form, Board $board = null) {
-            if (null != $board) {
-                $caseFormType = $board->getCaseFormType();
-                $form->add('caseEntity', 'App\\Form\\'.$caseFormType, [
-                    'board' => $board,
+        $formModifier = function (FormInterface $form, Municipality $municipality = null, FormBuilderInterface $builder) {
+            if (null != $municipality) {
+//                $builder->getForm()->add('board', EntityType::class, [
+//                    'class' => Board::class,
+//                    'label' => $this->translator->trans('Board', [], 'case'),
+//                    'placeholder' => $this->translator->trans('Choose a board', [], 'case'),
+//                ]);
+
+
+
+                $form->add('board', ChoiceType::class, [
+                    'choices' => $municipality->getBoards(),
+                    'label' => $this->translator->trans('Board', [], 'case'),
+                    'placeholder' => $this->translator->trans('Choose a board', [], 'case'),
+                    'choice_label' => fn (Board $board) => $board->getName(),
                 ]);
             } else {
-                $form->add('caseEntity', HiddenType::class);
+//                $builder->getForm()->add('board', HiddenType::class);
+                $form->add('board', HiddenType::class);
             }
         };
 
         $builder->addEventListener(
             FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($formModifier) {
-                $formModifier($event->getForm(), $event->getData());
+            function (FormEvent $event) use ($formModifier, $builder) {
+                $formModifier($event->getForm(), $event->getData(), $builder);
             }
         );
 
-        $builder->get('board')->addEventListener(
+        $builder->get('municipality')->addEventListener(
             FormEvents::POST_SUBMIT,
-            function (FormEvent $event) use ($formModifier) {
-                $board = $event->getForm()->getData();
-
-                $formModifier($event->getForm()->getParent(), $board);
+            function (FormEvent $event) use ($formModifier, $builder) {
+                $municipality = $event->getForm()->getData();
+                $formModifier($event->getForm()->getParent(), $municipality, $builder);
             }
         );
     }
