5d4f6bbdbd92be659363e7817b19affa1ec642b5
jekuaitk <jekua@aarhus.dk>
2022-02-02 14:47:06 +0100

    TVIST1-502: Working double dynamic case creation

--- assets/case/new.js
+++ assets/case/new.js
@@ -1,39 +1,55 @@
 /* global $, Event */
 
-const $municipality = $('#case_entity_municipality')
-$municipality.change(function () {
-  const $form = $(this).closest('form')
-  const data = {}
-  data[$municipality.attr('name')] = $municipality.val()
-  // Submit data via AJAX to the form's action path.
-  $.ajax({
-    url: $form.attr('action'),
-    type: $form.attr('method'),
-    data: data,
-    success: function (html) {
-      $('#case_entity_board').replaceWith(
-        $(html).find('#case_entity_board')
-      )
-      window.dispatchEvent(new Event('ajaxload'))
-    }
+
+window.addEventListener('ajaxload', function () {
+
+  const $municipality = $('#case_entity_municipality')
+
+  $municipality
+    .off('change')
+    .on('change', function () {
+    const $form = $(this).closest('form')
+    const data = {}
+    data[$municipality.attr('name')] = $municipality.val()
+    // Submit data via AJAX to the form's action path.
+    $.ajax({
+      url: $form.attr('action'),
+      type: $form.attr('method'),
+      data: data,
+      success: function (html) {
+        $('#case_entity_board').replaceWith(
+          $(html).find('#case_entity_board')
+        )
+
+        $('#case_entity_caseEntity').replaceWith(
+          $(html).find('#case_entity_caseEntity')
+        )
+
+        window.dispatchEvent(new Event('ajaxload'))
+      }
+    })
   })
+
+  const $board = $('#case_entity_board')
+  $board
+    .off('change')
+    .on('change', function () {
+    const $form = $(this).closest('form')
+    const data = {}
+    data[$board.attr('name')] = $board.val()
+    data[$municipality.attr('name')] = $municipality.val()
+    // Submit data via AJAX to the form's action path.
+    $.ajax({
+      url: $form.attr('action'),
+      type: $form.attr('method'),
+      data: data,
+      success: function (html) {
+        $('#case_entity_caseEntity').replaceWith(
+          $(html).find('#case_entity_caseEntity')
+        )
+        window.dispatchEvent(new Event('ajaxload'))
+      }
+    })
+  })
+
 })
-//
-// const $board = $('#case_entity_board')
-// $board.change(function () {
-//   const $form = $(this).closest('form')
-//   const data = {}
-//   data[$board.attr('name')] = $board.val()
-//   // Submit data via AJAX to the form's action path.
-//   $.ajax({
-//     url: $form.attr('action'),
-//     type: $form.attr('method'),
-//     data: data,
-//     success: function (html) {
-//       $('#case_entity_caseEntity').replaceWith(
-//         $(html).find('#case_entity_caseEntity')
-//       )
-//       window.dispatchEvent(new Event('ajaxload'))
-//     }
-//   })
-// })
--- src/Form/CaseEntityType.php
+++ src/Form/CaseEntityType.php
@@ -4,9 +4,9 @@ namespace App\Form;
 
 use App\Entity\Board;
 use App\Entity\Municipality;
+use App\Repository\BoardRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
@@ -16,14 +16,8 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class CaseEntityType extends AbstractType
 {
-    /**
-     * @var TranslatorInterface
-     */
-    private $translator;
-
-    public function __construct(TranslatorInterface $translator)
+    public function __construct(private TranslatorInterface $translator, private BoardRepository $boardRepository)
     {
-        $this->translator = $translator;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -34,40 +28,83 @@ class CaseEntityType extends AbstractType
             'placeholder' => $this->translator->trans('Choose a municipality', [], 'case'),
         ]);
 
-        $formModifier = function (FormInterface $form, Municipality $municipality = null, FormBuilderInterface $builder) {
-            if (null != $municipality) {
-//                $builder->getForm()->add('board', EntityType::class, [
-//                    'class' => Board::class,
-//                    'label' => $this->translator->trans('Board', [], 'case'),
-//                    'placeholder' => $this->translator->trans('Choose a board', [], 'case'),
-//                ]);
+        // Adds or hides form children based on provided municipality_id
+        $addBoardFormModifier = function (FormInterface $form, $municipality_id = null) {
+            if (null != $municipality_id) {
+                // No municipality chosen - show board form child
+                $boardChoices = $this->boardRepository->findBy(['municipality' => $municipality_id]);
 
-
-
-                $form->add('board', ChoiceType::class, [
-                    'choices' => $municipality->getBoards(),
-                    'label' => $this->translator->trans('Board', [], 'case'),
+                $form->add('board', EntityType::class, [
+                    'class' => Board::class,
+                    'choice_label' => 'name',
+                    'choices' => $boardChoices,
                     'placeholder' => $this->translator->trans('Choose a board', [], 'case'),
-                    'choice_label' => fn (Board $board) => $board->getName(),
                 ]);
             } else {
-//                $builder->getForm()->add('board', HiddenType::class);
+                // No municipality chosen - hide board and caseEntity form children
                 $form->add('board', HiddenType::class);
+                $form->add('caseEntity', HiddenType::class, [
+                    'mapped' => false,
+                ]);
             }
         };
 
+        // Base event listener for adding board child to form
+        // @see https://symfony.com/doc/5.4/form/dynamic_form_modification.html#dynamic-generation-for-submitted-forms
         $builder->addEventListener(
             FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($formModifier, $builder) {
-                $formModifier($event->getForm(), $event->getData(), $builder);
+            function (FormEvent $event) use ($addBoardFormModifier) {
+                $municipality = $event->getData()->getMunicipality();
+                $municipality_id = $municipality?->getId();
+                $addBoardFormModifier($event->getForm(), $municipality_id);
             }
         );
 
-        $builder->get('municipality')->addEventListener(
-            FormEvents::POST_SUBMIT,
-            function (FormEvent $event) use ($formModifier, $builder) {
-                $municipality = $event->getForm()->getData();
-                $formModifier($event->getForm()->getParent(), $municipality, $builder);
+        $builder->addEventListener(
+            FormEvents::PRE_SUBMIT,
+            function (FormEvent $event) use ($addBoardFormModifier) {
+                $data = $event->getData();
+                $municipality_id = array_key_exists('municipality', $data) ? $data['municipality'] : null;
+                $addBoardFormModifier($event->getForm(), $municipality_id);
+            }
+        );
+
+        // Adds or hides caseEntity form child based on provided board_id
+        $addCaseFormModifier = function (FormInterface $form, $board_id = null) {
+            if (null != $board_id) {
+                // Board chosen - show caseEntity form child
+                $board = $this->boardRepository->findOneBy(['id' => $board_id]);
+                $caseFormType = $board->getCaseFormType();
+
+                $form->add('caseEntity', 'App\\Form\\'.$caseFormType, [
+                    'mapped' => false,
+                    'board' => $board,
+                ]);
+            } else {
+                // No board chosen - hide caseEntity form child
+                $form->add('caseEntity', HiddenType::class, [
+                    'mapped' => false,
+                ]);
+            }
+        };
+
+        // Base event listener for adding caseEntity child to form
+        // @see https://symfony.com/doc/5.4/form/dynamic_form_modification.html#dynamic-generation-for-submitted-forms
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) use ($addCaseFormModifier) {
+                $board = $event->getData()->getBoard();
+                $board_id = $board?->getId();
+                $addCaseFormModifier($event->getForm(), $board_id);
+            }
+        );
+
+        $builder->addEventListener(
+            FormEvents::PRE_SUBMIT,
+            function (FormEvent $event) use ($addCaseFormModifier) {
+                $data = $event->getData();
+                $board_id = array_key_exists('board', $data) ? $data['board'] : null;
+                $addCaseFormModifier($event->getForm(), $board_id);
             }
         );
     }
