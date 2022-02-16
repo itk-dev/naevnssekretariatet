9c4903927851c057dbfabe611d8ff9e73cf8196b
jekuaitk <jekua@aarhus.dk>
2022-02-16 14:53:50 +0100

    TVIST1-510: Added and updated some translations

--- src/Controller/Admin/MailTemplateMacroCrudController.php
+++ src/Controller/Admin/MailTemplateMacroCrudController.php
@@ -9,10 +9,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MailTemplateMacroCrudController extends AbstractCrudController
 {
-    public function __construct(private MailTemplateHelper $mailTemplateHelper)
+    public function __construct(private MailTemplateHelper $mailTemplateHelper, private TranslatorInterface $translator)
     {
     }
 
@@ -33,12 +34,12 @@ class MailTemplateMacroCrudController extends AbstractCrudController
     {
         yield TextField::new('name');
         yield TextField::new('macro')
-            ->setHelp('The value to insert to use the macro in a template, i.e. if the name is <code>name_and_address</code> then <code>${name_and_address}</code> can be used in the template.')
+            ->setHelp($this->translator->trans('The value to insert to use the macro in a template, i.e. if the name is <code>name_and_address</code> then <code>${name_and_address}</code> can be used in the template.', [], 'admin'))
         ;
         yield TextareaField::new('content')
             ->setTemplatePath('admin/mail_template_macro/content.html.twig')
             ->setSortable(false)
-            ->setHelp('The content of the expanded macro.')
+            ->setHelp($this->translator->trans('The content of the expanded macro.', [], 'admin'))
         ;
         yield ChoiceField::new('templateTypes')
             ->setFormTypeOptions([
@@ -46,7 +47,7 @@ class MailTemplateMacroCrudController extends AbstractCrudController
                 'expanded' => true,
             ])
             ->setChoices($this->mailTemplateHelper->getMailTemplateTypeChoices())
-            ->setHelp('Select template types that this macro can be used for. If no template types are selected, the macro can be used in all templates.')
+            ->setHelp($this->translator->trans('Select template types that this macro can be used for. If no template types are selected, the macro can be used in all templates.', [], 'admin'))
         ;
     }
 }
