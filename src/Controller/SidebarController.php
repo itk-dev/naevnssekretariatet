03ce905e7ea095e56aa4027c0b21f085519d9a19
jekuaitk <jekua@aarhus.dk>
2022-02-18 09:38:37 +0100

    TVIST1-511: Hid inspection and inspection letter

--- src/Controller/SidebarController.php
+++ src/Controller/SidebarController.php
@@ -152,9 +152,10 @@ class SidebarController extends AbstractController
         // $submenuItems[] = $this->generateAgendaItemSubmenuItem($this->translator->trans('Case presentation', [], 'sidebar'), ['agenda_case_item_presentation'], $agendaId, $agendaItemId, $activeRoute),
         // $submenuItems[] = $this->generateAgendaItemSubmenuItem($this->translator->trans('Decision proposal', [], 'sidebar'), ['agenda_case_item_decision_proposal'], $agendaId, $agendaItemId, $activeRoute),
 
-        if ($isInspection && ($isCaseworker || $isAdministration)) {
-            $submenuItems[] = $this->generateAgendaItemSubmenuItem($this->translator->trans('Inspection', [], 'sidebar'), ['agenda_case_item_inspection', 'agenda_case_item_inspection_letter'], $agendaId, $agendaItemId, $activeRoute);
-        }
+        //TODO: For Phase one they will not be making inspection letters through TVIST1
+//        if ($isInspection && ($isCaseworker || $isAdministration)) {
+//            $submenuItems[] = $this->generateAgendaItemSubmenuItem($this->translator->trans('Inspection', [], 'sidebar'), ['agenda_case_item_inspection', 'agenda_case_item_inspection_letter'], $agendaId, $agendaItemId, $activeRoute);
+//        }
 
         $submenuItems[] = $this->generateSubmenuItem($this->translator->trans('Back to agenda', [], 'sidebar'), ['agenda_show'], $agendaId, $activeRoute);
 
