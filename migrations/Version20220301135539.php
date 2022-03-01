0da3bedf3029ce1afc4be59b14dd5d6c2b8b7981
jekuaitk <jekua@aarhus.dk>
2022-03-01 15:11:18 +0100

    TVIST1-518: Ensured existing HearingPosts are turned into HearingPostRequests via migrations

--- migrations/Version20220301135539.php
+++ migrations/Version20220301135539.php
@@ -31,18 +31,21 @@ final class Version20220301135539 extends AbstractMigration
         $this->addSql('ALTER TABLE hearing_post DROP FOREIGN KEY FK_54293ABF5DA0FB8');
         $this->addSql('DROP INDEX IDX_54293ABFE92F8F78 ON hearing_post');
         $this->addSql('DROP INDEX IDX_54293ABF5DA0FB8 ON hearing_post');
+        $this->addSql('INSERT INTO hearing_post_request (id, recipient_id, template_id, forwarded_on, title) SELECT id, recipient_id, template_id, forwarded_on, title FROM hearing_post');
         $this->addSql('ALTER TABLE hearing_post DROP recipient_id, DROP template_id, DROP forwarded_on, CHANGE title discr VARCHAR(255) NOT NULL');
+        $this->addSql('UPDATE hearing_post SET discr = \'hearingPostRequest\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE hearing_post_request');
-        $this->addSql('DROP TABLE hearing_post_response');
         $this->addSql('ALTER TABLE hearing_post ADD recipient_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', ADD template_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', ADD forwarded_on DATE DEFAULT NULL, ADD title VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP discr');
+        $this->addSql('UPDATE hearing_post INNER JOIN hearing_post_request ON hearing_post.id = hearing_post_request.id SET hearing_post.recipient_id = hearing_post_request.recipient_id, hearing_post.template_id = hearing_post_request.template_id, hearing_post.forwarded_on = hearing_post_request.forwarded_on, hearing_post.title = hearing_post_request.title');
         $this->addSql('ALTER TABLE hearing_post ADD CONSTRAINT FK_54293ABFE92F8F78 FOREIGN KEY (recipient_id) REFERENCES party (id)');
         $this->addSql('ALTER TABLE hearing_post ADD CONSTRAINT FK_54293ABF5DA0FB8 FOREIGN KEY (template_id) REFERENCES mail_template (id)');
         $this->addSql('CREATE INDEX IDX_54293ABFE92F8F78 ON hearing_post (recipient_id)');
         $this->addSql('CREATE INDEX IDX_54293ABF5DA0FB8 ON hearing_post (template_id)');
+        $this->addSql('DROP TABLE hearing_post_request');
+        $this->addSql('DROP TABLE hearing_post_response');
     }
 }
