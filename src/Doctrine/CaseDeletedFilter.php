af14cc3a60daa773a7e81fd301b2401ecd0d946a
jekuaitk <jekua@aarhus.dk>
2022-03-03 15:49:32 +0100

    TVIST1-518: Added doctrine filter for deleted cases

---
+++ src/Doctrine/CaseDeletedFilter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Doctrine;
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Query\Filter\SQLFilter;
+
+class CaseDeletedFilter extends SQLFilter
+{
+    public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias)
+    {
+        if ('App\Entity\CaseEntity' != $targetEntity->getReflectionClass()->name) {
+            return '';
+        }
+
+        return sprintf('%s.soft_deleted = false', $targetTableAlias);
+    }
+}
