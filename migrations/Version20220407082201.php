e31016ab108430b157b2ddaffeb0a81e5439278a
jekuaitk <jekua@aarhus.dk>
2022-04-07 11:00:04 +0200

    TVIST1: Update auto-generated migration

--- migrations/Version20220407082201.php
+++ migrations/Version20220407082201.php
@@ -20,7 +20,7 @@ final class Version20220407082201 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE board_member ADD address_number VARCHAR(255) NOT NULL, ADD address_floor VARCHAR(255) DEFAULT NULL, ADD address_side VARCHAR(255) DEFAULT NULL, ADD address_postal_code INT NOT NULL, ADD address_city VARCHAR(255) NOT NULL, ADD address_validated_at DATETIME DEFAULT NULL, ADD address_bbr_data LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\', ADD address_extra_information VARCHAR(255) DEFAULT NULL, ADD identification_type VARCHAR(32) NOT NULL, ADD identification_identifier VARCHAR(255) NOT NULL, ADD identification_p_number VARCHAR(10) DEFAULT NULL, ADD identification_validated_at DATETIME DEFAULT NULL, CHANGE cpr address_street VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE board_member ADD address_number VARCHAR(255) NOT NULL, ADD address_street VARCHAR(255) NOT NULL, ADD address_floor VARCHAR(255) DEFAULT NULL, ADD address_side VARCHAR(255) DEFAULT NULL, ADD address_postal_code INT NOT NULL, ADD address_city VARCHAR(255) NOT NULL, ADD address_validated_at DATETIME DEFAULT NULL, ADD address_bbr_data LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\', ADD address_extra_information VARCHAR(255) DEFAULT NULL, ADD identification_type VARCHAR(32) NOT NULL, ADD identification_identifier VARCHAR(255) NOT NULL, ADD identification_p_number VARCHAR(10) DEFAULT NULL, ADD identification_validated_at DATETIME DEFAULT NULL, DROP cpr');
     }
 
     public function down(Schema $schema): void
