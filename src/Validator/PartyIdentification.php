c9b56588f0deb1f010ced716f095b8243cdd4ebb
jekuaitk <jekua@aarhus.dk>
2022-04-22 09:43:32 +0200

    TVIST1: Translations

--- src/Validator/PartyIdentification.php
+++ src/Validator/PartyIdentification.php
@@ -9,9 +9,4 @@ use Symfony\Component\Validator\Constraint;
  */
 class PartyIdentification extends Constraint
 {
-    /*
-     * Any public properties become valid options for the annotation.
-     * Then, use these in your validator class.
-     */
-    public $message = 'The value "{{ value }}" is not a valid CPR or CVR number.';
 }
--- src/Validator/PartyIdentificationValidator.php
+++ src/Validator/PartyIdentificationValidator.php
@@ -5,9 +5,14 @@ namespace App\Validator;
 use App\Entity\Embeddable\Identification;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PartyIdentificationValidator extends ConstraintValidator
 {
+    public function __construct(private TranslatorInterface $translator)
+    {
+    }
+
     public function validate($value, Constraint $constraint)
     {
         /* @var $constraint \App\Validator\PartyIdentification */
@@ -22,8 +27,9 @@ class PartyIdentificationValidator extends ConstraintValidator
 
         // Ensure identifier looks like a CPR or CVR number
         if (!preg_match('/^\d{6}\-?\d{4}$|^\d{8}$/', $value->getIdentifier(), $matches)) {
-            $this->context->buildViolation($constraint->message)
-                ->setParameter('{{ value }}', $value->getIdentifier())
+            $message = $this->translator->trans('The value { value } is not a valid CPR or CVR number.', ['{ value }' => $value->getIdentifier()], 'validator');
+            $this->context->buildViolation($message)
+//                ->setParameter('{ value }', $value->getIdentifier())
                 ->addViolation()
             ;
         }
