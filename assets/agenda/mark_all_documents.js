55e10a5d0d1b630434ba0a0173185878161ff41c
jekuaitk <jekua@aarhus.dk>
2022-05-09 15:07:08 +0200

    TVIST1: Code cleanup

--- assets/agenda/mark_all_documents.js
+++ assets/agenda/mark_all_documents.js
@@ -1,9 +1,9 @@
 /* global $ */
 
 $(function () {
-  const $markAllButton = $('#markAllDocumentsButton')
-
-  $markAllButton.on('click', function () {
-    $('input:checkbox').prop('checked', true)
+  // When an element with an all-elements-selector attribute is clicked …
+  $('[data-all-elements-selector]').on('click', function () {
+    // … we check all elements targeted by the selector.
+    $($(this).data('all-elements-selector')).prop('checked', true)
   })
 })
