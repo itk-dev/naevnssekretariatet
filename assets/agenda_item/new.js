655f8fb2f01216ccf530194a17bad7829e038bbf
jekuaitk <jekua@aarhus.dk>
2022-08-04 10:18:48 +0200

    TVIST1-588: Automatically filled out AgendaCaseItem meeting point if it is to be inspected

--- assets/agenda_item/new.js
+++ assets/agenda_item/new.js
@@ -1,7 +1,7 @@
 /* global $ */
 
 const $type = $('#agenda_item_type')
-$type.change(function () {
+$type.on('change', function () {
   const $form = $(this).closest('form')
   const data = {}
   data[$type.attr('name')] = $type.val()
@@ -14,6 +14,19 @@ $type.change(function () {
       $('#agenda_item_agendaItem').replaceWith(
         $(html).find('#agenda_item_agendaItem')
       )
+
+      if ($type.val() === 'App\\Entity\\AgendaCaseItem') {
+        const $agendaCaseItem = $('#agenda_item_agendaItem_caseEntity')
+        $agendaCaseItem.on('change', function () {
+          const $caseDescription = $agendaCaseItem.find(':selected').text()
+          const $meetingPoint = $("input[id='agenda_item_agendaItem_meetingPoint']")
+          if ($caseDescription.includes('Besigtigelse')) {
+            $meetingPoint.val($caseDescription.substring($caseDescription.lastIndexOf('-') + 2))
+          } else {
+            $meetingPoint.val('')
+          }
+        })
+      }
     }
   })
 })
