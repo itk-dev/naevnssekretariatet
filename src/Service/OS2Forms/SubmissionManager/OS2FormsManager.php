d9b623cbbf479b36b33df3d815234aea20a9760a
jekuaitk <jekua@aarhus.dk>
2022-10-04 13:33:46 +0200

    TVIST1-21: Allowed creation of RentBoardCases via OS2Forms

--- src/Service/OS2Forms/SubmissionManager/OS2FormsManager.php
+++ src/Service/OS2Forms/SubmissionManager/OS2FormsManager.php
@@ -7,7 +7,7 @@ use App\Service\HearingHelper;
 
 class OS2FormsManager
 {
-    public function __construct(private CaseManager $caseManager, private HearingHelper $hearingHelper, private ResidentComplaintBoardCaseTypeManager $residentComplaintBoardCaseTypeManager, private HearingResponseManager $hearingResponseManager)
+    public function __construct(private CaseManager $caseManager, private HearingHelper $hearingHelper, private ResidentComplaintBoardCaseTypeManager $residentComplaintBoardCaseTypeManager, private RentComplaintBoardCaseTypeManager $rentComplaintBoardCaseTypeManager, private HearingResponseManager $hearingResponseManager)
     {
     }
 
@@ -19,6 +19,7 @@ class OS2FormsManager
         // TODO: Needs updating when form ids are finalized and hearing response forms are added.
         match ($webformId) {
             'tvist1_beboerklagenaevnet_ny_sag' => $this->caseManager->handleOS2FormsCaseSubmission($sender, $submissionData, $this->residentComplaintBoardCaseTypeManager),
+            'tvist1_huslejenaevnet_ny_sag' => $this->caseManager->handleOS2FormsCaseSubmission($sender, $submissionData, $this->rentComplaintBoardCaseTypeManager),
             'tvist1_hoeringssvar_test' => $this->hearingHelper->handleOS2FormsHearingSubmission($sender, $submissionData, $this->hearingResponseManager),
         };
     }
