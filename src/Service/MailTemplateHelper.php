55ef51b1fc52de2f5e93709feb7f088d2326919b
jekuaitk <jekua@aarhus.dk>
2023-01-05 15:05:52 +0100

    TVIST1-735: Cleanup

--- src/Service/MailTemplateHelper.php
+++ src/Service/MailTemplateHelper.php
@@ -260,7 +260,7 @@ class MailTemplateHelper
 
     public function getTemplates(string $type): array
     {
-        return $this->mailTemplateRepository->findBy(['type' => $type, 'isArchived' => 0], ['name' => 'ASC']);
+        return $this->mailTemplateRepository->findBy(['type' => $type, 'isArchived' => false], ['name' => 'ASC']);
     }
 
     private static array $blockElementClasses = [
