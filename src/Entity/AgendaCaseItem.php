11f84ed88b012af6129a245759cef150ba1d7cbf
jekuaitk <jekua@aarhus.dk>
2025-01-13 13:28:36 +0100

    3557: Applied coding standards

--- src/Entity/AgendaCaseItem.php
+++ src/Entity/AgendaCaseItem.php
@@ -26,6 +26,7 @@ class AgendaCaseItem extends AgendaItem
 
     /**
      * @ORM\ManyToMany(targetEntity=Document::class, inversedBy="agendaCaseItems")
+     *
      * @ORM\OrderBy({"uploadedAt" = "ASC"})
      */
     private $documents;
