ff38269733fc02e63e55fa02cbab3986d6713138
jekuaitk <jekua@aarhus.dk>
2026-06-01 10:53:41 +0200

    Use adressevaelger for address autocompletion

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,16 @@ about writing changes to this log.
 
 ## [Unreleased]
 
+* Replaced the deprecated `dawa-autocomplete2` library with Klimadatastyrelsens's
+  `adressevaelger` for client-side address lookup.
+
+* NOTE: `AddressHelper::fetchAddressData()` still calls
+ `https://api.dataforsyningen.dk/datavask/adresser`, which shuts down on
+ **2026-08-17** together with the rest of DAWA. The replacement has yet to
+  be released, cf. [Addressevask](https://confluence.sdfi.dk/display/ADV/Adressevask).
+  That means, address validation (the `case_validate_address` action and the
+  BBR data flow in `BBRHelper`) will stop working **2026-08-17**.
+
 ## [1.10.1] - 2026-05-21
 
 * [PR-416](https://github.com/itk-dev/naevnssekretariatet/pull/416)
--- src/Form/Embeddable/AddressLookupType.php
+++ src/Form/Embeddable/AddressLookupType.php
@@ -5,30 +5,43 @@ namespace App\Form\Embeddable;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * Address field with DAWA addres look up.
+ * Address field with adressevælger address lookup.
  *
- * @see https://autocomplete.aws.dk/guide2.html
+ * @see https://github.com/Klimadatastyrelsen/adressevaelger
  */
 class AddressLookupType extends AddressType
 {
+    public function __construct(
+        TranslatorInterface $translator,
+        private readonly string $adressevaelgerToken,
+        private readonly string $adressevaelgerApiUrl,
+    ) {
+        parent::__construct($translator);
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         parent::configureOptions($resolver);
         $resolver
             ->setRequired('lookup-placeholder')
             ->setDefault('lookup-help', null)
+            ->setDefault('lookup-kommune-kode', null)
         ;
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         parent::buildView($view, $form, $options);
-        $view->vars['attr']['data-dawa-address-lookup'] = json_encode(array_filter([
+        $view->vars['attr']['data-address-lookup'] = json_encode(array_filter([
             'placeholder' => $options['lookup-placeholder'],
             'help' => $options['lookup-help'],
             'selector-pattern' => sprintf('[name="%s[%%name%%]"]', $view->vars['full_name']),
+            'token' => $this->adressevaelgerToken,
+            'api-url' => $this->adressevaelgerApiUrl ?: null,
+            'kommune-kode' => $options['lookup-kommune-kode'],
         ]), JSON_THROW_ON_ERROR);
     }
 }
